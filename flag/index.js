f0337122b5d094b1b91606a68547183de2b3e0c4
jingzi <1163478123@qq.com>
2021-08-11 11:30:51 +0800

    国旗图片处理为精灵图，并且获取位置信息

--- flag/index.js
+++ flag/index.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
-const CryptoJS = require("crypto-js");
 const path = require('path');
 const axios = require('axios');
 const entities = require('html-entities');
 const cheerio = require('cheerio'); //用在DOM进行操作的地方
+//获取国旗的名字
+const getFlagNames = ()=>{
+  const pathName="flag/img";
+  let names = [];
+  fs.readdir(pathName, function(err, files){
+    files.forEach(file => {
+      names.push(`"${file}"`);
+    });
+    console.log(names.length)
+    fs.writeFile("flag/data.js", `[${names}]`, err => {
+      if (err) {
+          console.log(err);
+      }
+      console.log("写入成功！");
+    });
+  })
+}
+getFlagNames();
